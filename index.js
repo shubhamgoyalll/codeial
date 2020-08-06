a3ed80853e09089a8bd671e57f2e97a57cc86001
shubham <goyalshubham2521@gmail.com>
2020-08-07 01:18:26 +0530

    First Commit : create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,13 @@
+// make index.js then npm init then npm install express
+
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log(`Error in running on server, ${err}`);
+    }
+    console.log(`Server is running on port, ${port}`);
+});
